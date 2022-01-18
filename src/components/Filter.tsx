dbd38407fed02264d2dd368d0dad0e70572bd577
Valentino <valentino.chiola@eggcooperation.com>
2022-01-18 15:44:00 -0300

    Add filter to the search

---
+++ src/components/Filter.tsx
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react'
+
+export const Filter = ({ onSubmit, onChange, style }: Props) => {
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  return (
+    <nav style={style} className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <form className="d-flex" onSubmit={(e) => {
+          e.preventDefault()
+          onSubmit?.(inputRef)
+        }}>
+          <input
+            ref={inputRef}
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            onChange={() => onChange?.(inputRef)}
+          />
+          {onSubmit && <button className="btn btn-outline-success" type="submit">Search</button>}
+        </form>
+      </div>
+    </nav>
+  )
+}
+
+interface Props {
+  onSubmit?: (ref: React.RefObject<HTMLInputElement>) => void
+  onChange?: (ref: React.RefObject<HTMLInputElement>) => void
+  style?: React.CSSProperties
+}
